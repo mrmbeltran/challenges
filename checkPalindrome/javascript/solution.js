7e8d2d9f913535690e5d7babf0dc85cf5bf36415
Marlon Beltran <mbeltran@marencoventiladores.com>
2022-03-05 08:50:02 -0600

    checkPalindrome js

--- checkPalindrome/javascript/solution.js
+++ checkPalindrome/javascript/solution.js
@@ -1,15 +1,19 @@
+/**
+ * Takes an string and then check if it isn't empty, after that verify if is palindrome.
+ *
+ * @param {string} inputString
+ * @return {boolean}
+ */
+const solution = (inputString) => {
+    const inputString = inputString.toLowerCase();
 
-function solution(inputString) {
-    inputString = inputString.toLowerCase();
-    let _length = inputString.length;
-
-    if ( 1 <= _length && _length <= Math.pow(10, 5)) {    
+    if (1 <= inputString.length && inputString.length <= Math.pow(10, 5)) {    
         for (let i = 0; i < _length; i++) {
             if (inputString[i] != inputString[_length - ( ! i ? 1 : i +1)]) {
                 return false;
             }
         }
-
-        return true;
     }
+
+    return true;
 }
